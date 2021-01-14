2330cc895b9b1e2a351a93a539df5984511135a1
Jethro A <jet.arciaga@gmail.com>
2021-01-14 18:22:39 +0800

    Add order_checkbox, hover_print and hover_next. Access hidden element
    Use ActionChains(driver).move_to_element(hover_print) to access
    hidden element/XPATH of hover_document. It won't appear unless
    hovered
    
    Printing needs manual intervention. No way to automate print
    dialog box.

--- login_lazada.py
+++ login_lazada.py
@@ -4,10 +4,12 @@ import json
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+
 
 file = os.path.join('..', 'credentials', 'log_lazada.json')
 with open(file, 'r') as login_details:
@@ -53,6 +55,12 @@ for i in range(1,6): #close announcement pop-up then go to pending orders
         pending_orders.click()
         break
     except:
+        element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((
+        By.CLASS_NAME, 'next-dialog-close'))
+        )
+        driver.get(driver.current_url)
+        time.sleep(5)
         close_announcement = driver.find_element_by_class_name(
             'next-dialog-close'
             )
@@ -60,8 +68,32 @@ for i in range(1,6): #close announcement pop-up then go to pending orders
         print('Closing Announcement')
     time.sleep(5)
 print('Going to Pending Orders page...')
-"""
-close_reminder = driver.find_element_by_class_name(
-    'next-icon next-icon-close next-icon-small')
-close_reminder.click()
-"""
+
+for i in range(1,6):
+    if len(driver.window_handles) == 2:
+        driver.switch_to.window(driver.window_handles[1])
+        time.sleep(5)
+        close_reminder = driver.find_element_by_class_name(
+            'next-dialog-close')
+        close_reminder.click()
+        time.sleep(3)
+        break
+    else:
+        print('Current tabs open ' + str(len(driver.window_handles)))
+        time.sleep(3)
+
+print('You are now in order page...')
+
+order_checkbox = driver.find_element_by_xpath("(//input[@type='checkbox'])[1]")
+order_checkbox.click()
+hover_print = driver.find_element_by_xpath("(//button[@type='button'])[2]")
+hover = ActionChains(driver).move_to_element(hover_print)
+hover.perform()
+time.sleep(3)
+#separate action xpath will only appear when hover_print is mouse over
+hover_document = driver.find_element_by_xpath(
+    "//ul[@class='next-menu-content']/li[position()=1]"
+)
+hover_next = ActionChains(driver).move_to_element(hover_document).click()
+hover_next.perform()
+print("Printing needs manual intervention")
