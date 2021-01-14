a2921e1b74f40d344f03b9b539d8ca7184a7c478
Jethro A <jet.arciaga@gmail.com>
2021-01-14 10:13:10 +0800

    Add gitignore for geckodriver, add functions and code until order pending

--- login_lazada.py
+++ login_lazada.py
@@ -12,55 +12,56 @@ from selenium.common.exceptions import TimeoutException
 file = os.path.join('..', 'credentials', 'log_lazada.json')
 with open(file, 'r') as login_details:
     login = json.load(login_details)
-baseUrl = 'https://sellercenter.lazada.com.ph'
+
 driver = webdriver.Firefox() #opens firefox browser
 
+def login_page():
+    try:
+        driver.get('https://sellercenter.lazada.com.ph')
+        time.sleep(2)
+    except:
+        driver.get(driver.current_url)
+        print('Refreshing page...')
+        time.sleep(2)
 
-driver.get(baseUrl)
-
-def element_wait():
-    element = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.XPATH, "//button[@type='button']"))
+def credentials():
+    element = WebDriverWait(driver,5).until(
+    EC.presence_of_element_located((By.XPATH,"//button[@type='button']"))
     )
+    email = driver.find_element_by_name('TPL_username')
+    email.send_keys(login['email'])
+    password = driver.find_element_by_name('TPL_password')
+    password.send_keys(login['password'])
+    password.send_keys(Keys.RETURN)
 
-while 'Seller' not in driver.title:
-    driver.get(driver.current_url)
-    time.sleep(5)
-    if 'Seller' in driver.title:
+for i in range(1,6): #loop if login page fail to load
+    if 'Seller' not in driver.title:
+        login_page()
+    else:
+        print('Login Attempts ' + str(i))
+        credentials()
         break
 
-try:
-    element_wait()
-except TimeoutException:
-    print('Refreshing page...')
-    driver.get(driver.current_url)
-    element_wait()
-
-assert 'Seller' in driver.title
-email = driver.find_element_by_name('TPL_username')
-email.send_keys(login['email'])
-password = driver.find_element_by_name('TPL_password')
-password.send_keys(login['password'])
-password.send_keys(Keys.RETURN)
 print('Login Successfully')
+time.sleep(5)
 
-close_className = 'next-dialog-close'
-
-while 'asc-task-item' not in driver.page_source:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-        (By.CLASS_NAME, close_className))
-    )
-    if 'next-overlay-backdrop' in driver.page_source:
+for i in range(1,6): #close announcement pop-up then go to pending orders
+    try:
+        pending_orders = driver.find_element_by_xpath(
+            "//div[@class='asc-task-list']/a[@class='asc-task-item aplus-auto-exp']"
+            )
+        pending_orders.click()
         break
-
-close_announcement = driver.find_element_by_class_name(
-    close_className
-    )
-close_announcement.click()
-
-pending_orders = driver.find_element_by_xpath(
-    "//div[@class='asc-task-list']/a[@class='asc-task-item aplus-auto-exp']"
-    )
-pending_orders.click()
+    except:
+        close_announcement = driver.find_element_by_class_name(
+            'next-dialog-close'
+            )
+        close_announcement.click()
+        print('Closing Announcement')
+    time.sleep(5)
 print('Going to Pending Orders page...')
+"""
+close_reminder = driver.find_element_by_class_name(
+    'next-icon next-icon-close next-icon-small')
+close_reminder.click()
+"""
